2d5443713f6fbc757b13cd8a3a2ed48611c489d2
loveleen4k <loveleen14k@gmail.com>
2024-04-10 12:42:46 +0530

    commit2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './header';
 import Details from './details';
 import './app.css';
